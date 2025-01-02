c719695cd1dd8f6d8934f088dffca53de6dfb808
Elsera05 <achmadrasyid53@gmail.com>
2025-01-02 19:29:49 +0700

    latihan variabel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 #[test]
 fn test_main() {
     println!("Hello, test!");
@@ -14,4 +10,81 @@ fn test_variable() {
     println!("hello,{}",name);
     name = "Rima Oktavianti";
     println!("hello,{}",name);    
+}
+
+
+#[test]
+fn shadowing (){
+    let name = "mamads";
+    println!("hello,{}",name);
+    let name = 10;
+    println!("hello,{}",name);
+}
+
+
+// arc belajar variabel
+
+#[test]
+fn nama_main (){
+    let name = "Achmad Rasyid ramadhan ";
+    println!("hello {}",name);
+}
+
+
+
+#[test]
+fn test_mutable() {
+    let mut age = 25;
+    println!("your age is ,{}",age);
+    age = 30;
+    println!("your age is ,{}",age);    
+}
+
+
+#[test]
+fn test_f64(){
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("x = {}, y = {}",x,y);
+}
+
+
+
+// fn greet(){
+//     println!("hello world",)
+// }
+
+// fn main() {
+//     println!("Hello, rust");
+//     greet();
+// }
+
+
+#[test]
+fn jumlah(){
+    let a = 10;
+    let b = 5;
+    let c = a - b;
+    println! ("hasilnya adalah {}",c);
+}
+
+
+
+#[test]
+fn tests_f64(){
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    let x_as_f32 = x as f32;
+    println!("x = {}, y = {}", x_as_f32, y);
+}
+
+
+
+fn greet(name: String) {
+    println!("Hello, {}!", name);
+}
+
+fn main() {
+    let name = String::from("Achmad");
+    greet(name);
 }
